9b6591f653adf73f5aa9d9da45fa263337cd2204
liqunhe27 <qtnvlh4@ucl.ac.uk>
2024-03-09 12:17:50 +0000

    Update audio_player.py

--- audio_player.py
+++ audio_player.py
@@ -21,7 +21,7 @@ def play(file_path: str):
             </audio>
             """
         # Simulate a delay to show the spinner
-        time.sleep(1)
+        time.sleep(2)
         # Render the audio player using Markdown
         placeholder.markdown(md, unsafe_allow_html=True)
 
